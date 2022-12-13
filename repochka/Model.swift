db6359f383b366c843f33bccf36ee1cf8bd5bde1
isaevk <kirill_isaev@aol.com>
2022-12-13 18:07:39 +0600

    new work 1.1

---
+++ repochka/Model.swift
@@ -0,0 +1,16 @@
+//
+//  Model.swift
+//  repochka
+//
+//  Created by Kirill Dev on 13.12.2022.
+//
+
+import Foundation
+
+struct ReverseredWord {
+     func rev(word: String) -> String? {
+         let word1 = String(word.reversed())
+        return word1
+    }
+    
+}
--- repochka/ViewController.swift
+++ repochka/ViewController.swift
@@ -8,12 +8,40 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+  var rev = ReverseredWord()
+    
+    let button = UIButton()
+    let label = UILabel()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        button.frame = CGRect(x: 100, y: 300, width: 200, height: 80)
+        button.backgroundColor = .red
+        button.setTitle("START", for: .normal)
+        button.addTarget(self, action: #selector(actionButton(_ :)), for: .touchUpInside)
+        view.addSubview(button)
+        
+        label.frame = CGRect(x: 150, y: 200, width: 100, height: 50)
+        label.text = "TEXT"
+        label.textColor = .white
+        label.backgroundColor = .blue
+        view.addSubview(label)
     }
-
-
+    
+    @objc func actionButton(_ sender: Any) {
+        let alert = UIAlertController(title: "Attention",
+                                      message: "Type of word",
+                                      preferredStyle: .alert)
+        let action = UIAlertAction(title: "Ok", style: .default) { [self] (action) in
+            let text = alert.textFields?.first
+            self.label.text = rev.rev(word: (text?.text)!)
+        }
+        
+        alert.addAction(action)
+        alert.addTextField()
+        present(alert, animated: true)
+        
+    }
 }
-
